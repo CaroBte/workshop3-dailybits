bc1452e39dc601b7b34ef45ae3ef214586a4cb23
CaroBte <karito0721@hotmail.com>
2023-02-01 12:19:16 -0500

    Profile and Login

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import '../styles/profile.sass';
+import { useAuth0 } from '@auth0/auth0-react'
+
 
 const Profile = () => {
 
+    const { logout, user, isAuthenticated } = useAuth0()
+
+    const handleLogout = () => {
+        logout({ returnTo: window.location.origin })
+    }
+
     return (
 
-        <div className='profile'>
-            <img src="https://cdn-icons-png.flaticon.com/512/5231/5231019.png" />
-            <h1>Hello, Caro Bustamante </h1>
-            <h2>Email: karito0721@gmail.com </h2>
-        </div>
+        <>
+            {isAuthenticated ? <div className='profile'>
+                {console.log(user)}
+                <img src={user.picture} alt={user.name} />
+                <h1>Hello, {user.name}</h1>
+                <h2>Email: {user.email}</h2>
+                <button onClick={handleLogout}>Cerrar sesión</button>
+            </div> : <div className='profile'>
+                <img src="https://cdn-icons-png.flaticon.com/512/5231/5231019.png" />
+                <h1>Hello, Bianey Bolaños </h1>
+                <h2>Email: bney@gmail.com </h2>
+                <button onClick={handleLogout}>Cerrar sesión</button>
+            </div>}
+        </>
 
-
-        /*         <div className='profile'>
-                    <img src={user.picture} alt={user.name} />
-                    <h1>Hello, {user.name}</h1>
-                    <h2>Email: {user.email}</h2>
-                </div> */
     )
 }
 
