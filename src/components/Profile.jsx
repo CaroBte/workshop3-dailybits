630d069e9d32c409822f4e680c7a0c9603d33119
CaroBte <karito0721@hotmail.com>
2023-02-01 10:21:24 -0500

    profile styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../styles/profile.sass';
 
 const Profile = () => {
+
     return (
-        <div>Profile</div>
+
+        <div className='profile'>
+            <img src="https://cdn-icons-png.flaticon.com/512/5231/5231019.png" />
+            <h1>Hello, Caro Bustamante </h1>
+            <h2>Email: karito0721@gmail.com </h2>
+        </div>
+
+
+        /*         <div className='profile'>
+                    <img src={user.picture} alt={user.name} />
+                    <h1>Hello, {user.name}</h1>
+                    <h2>Email: {user.email}</h2>
+                </div> */
     )
 }
 
