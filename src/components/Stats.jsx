6aff4b888a8d8f63d636d754b23e5ad32b3ed741
Jaredzu <jared.zu96@gmail.com>
2023-02-09 18:37:41 -0500

    feat: added Firebase deploy

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,21 +5,21 @@ const Stats = () => {
     return (
         <div>
             <h3 className='stats-text'>Estadísticas</h3>
-            <p className='stats-text'>Los últimos 7 días <i class="fa-solid fa-chevron-down"></i></p>
+            <p className='stats-text'>Los últimos 7 días <i className="fa-solid fa-chevron-down"></i></p>
             <div className='stats-item'>
-                <p><i class="fa-regular fa-clock"></i> Tiempo de estudio (horas)</p>
+                <p><i className="fa-regular fa-clock"></i> Tiempo de estudio (horas)</p>
                 <p>1</p>
             </div>
             <div className='stats-item'>
-                <p><i class="fa-regular fa-comment"></i> Respuestas contestadas</p>
+                <p><i className="fa-regular fa-comment"></i> Respuestas contestadas</p>
                 <p>40</p>
             </div>
             <div className='stats-item'>
-                <p><i class="fa-regular fa-comment"></i> Respuestas correctas</p>
+                <p><i className="fa-regular fa-comment"></i> Respuestas correctas</p>
                 <p className='stats-correct'>20</p>
             </div>
             <div className='stats-item'>
-                <p><i class="fa-regular fa-comment"></i> Respuestas incorrectas</p>
+                <p><i className="fa-regular fa-comment"></i> Respuestas incorrectas</p>
                 <p className='stats-incorrect'>20</p>
             </div>
 
