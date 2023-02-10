22306b927e32a472fa8756b6993fa7ee2315793e
CaroBte <karito0721@hotmail.com>
2023-02-09 19:45:48 -0500

    feat: added quest

--- src/components/Quest.jsx
+++ src/components/Quest.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import kid from '../imgs/kid.png'
+import '../styles/quest.sass'
 
 const Quest = () => {
     return (
-        <div>Quest</div>
+        <>
+            <div className="top-bar">
+                <i className="fa-solid fa-xmark"></i>
+                <div className='progress-bar'></div>
+            </div>
+            <div className="d-flex flex-row">
+                <img src={kid} />
+                <h4>¿Cuál es la forma más eficiente de aplicar estilos CSS en un documento HTML?</h4>
+            </div>
+
+        </>
     )
 }
 
