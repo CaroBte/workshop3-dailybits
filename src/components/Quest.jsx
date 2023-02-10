e7424ad950d216d6cdef2143a618c12c0dc7be1c
Jaredzu <jared.zu96@gmail.com>
2023-02-09 20:36:54 -0500

    feat: add Quest layout

--- src/components/Quest.jsx
+++ src/components/Quest.jsx
@@ -5,13 +5,40 @@ import '../styles/quest.sass'
 const Quest = () => {
     return (
         <>
-            <div className="top-bar">
-                <i className="fa-solid fa-xmark"></i>
-                <div className='progress-bar'></div>
-            </div>
-            <div className="d-flex flex-row">
-                <img src={kid} />
-                <h4>¿Cuál es la forma más eficiente de aplicar estilos CSS en un documento HTML?</h4>
+            <div>
+                <div className="top-bar">
+                    <i className="fa-solid fa-xmark"></i>
+                    <div className='progress-bar'></div>
+                    <div className=''>
+                        <span className='heart'>❤</span>
+                        <span className='lives-counter'>4</span>
+                    </div>
+                </div>
+                <div className='quest'>
+                    <img src={kid} />
+                    <h3>¿Cuál es la forma más eficiente de aplicar estilos CSS en un documento HTML?</h3>
+                </div>
+                <div className='options'>
+                    <div className='stats-item option'>
+                        <p>En Línea</p>
+                        <i className="fa-regular fa-circle"></i>
+                    </div>
+                    <div className='stats-item option'>
+                        <p>Incrustado en la cabecera</p>
+                        <i className="fa-regular fa-circle"></i>
+                    </div>
+                    <div className='stats-item option'>
+                        <p>Hojas de estilo externas</p>
+                        <i className="fa-regular fa-circle"></i>
+                    </div>
+                    <div className='stats-item option'>
+                        <p>Mediante PHP</p>
+                        <i className="fa-regular fa-circle"></i>
+                    </div>
+
+                    <button id='btn-comprobar'>COMPROBAR</button>
+                </div>
+
             </div>
 
         </>
